488159f54ab4e5a2deafcded986486169b1faf14
Tim <47ta@bk.ru>
2022-09-28 11:50:23 +0500

    HW

--- Homework1/Ex2.py
+++ Homework1/Ex2.py
@@ -1,23 +1,10 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
+input()
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not (x or y or z) == (not x and not y and not z))
+            print (x,y,z)
 
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print("Утверждение истинно")
-else:
-    print("Утверждение ложно")
+print("Утверждение истинно")
--- Homework1/Ex3.py
+++ Homework1/Ex3.py
@@ -5,33 +5,22 @@
 # - x=2; y=4 -> 1
 # - x=-34; y=-30 -> 3
 
-def inputKoord(x):
-    a = [0] * x
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = float(input(f"Введите {i+1} координату: "))
-                a[i] = number
-                is_OK = True
-                if a[i] == 0:
-                    is_OK = False
-                    print("Координата не должно быть равна 0 ")
-            except ValueError:
-                print("Ты ошибся. Вводить надо числа!")
-    return a
+x = int(input('Введите x:'))
+y = int(input('Введите y:'))
+
+if x == 0 and y == 0:
+    print('Точка лежит в начале координат')
+elif x == 0:
+    print('Точка лежит на оси Х')
+elif y == 0:
+    print('Точка лежит на оси Y')
 
 
-def checkCoordinates(xy):
-    text = 4
-    if xy[0] > 0 and xy[1] > 0:
-        text = 1
-    elif xy[0] < 0 and xy[1] > 0:
-        text = 2
-    elif xy[0] < 0 and xy[1] < 0:
-        text = 3
-    print(f"Точка находится в {text} четверти плоскости")
-
-
-koordinate = inputKoord(2)
-checkCoordinates(koordinate)
+if x > 0 and y > 0:
+    print('Точка лежит в 1 четверти')
+elif x < 0 and y < 0:
+    print('Точка лежит в 3 четверти')
+elif x > 0 and y < 0:
+    print('Точка лежит в 4 четверти')
+elif x < 0 and y > 0:
+    print('Точка лежит в 2 четверти')
