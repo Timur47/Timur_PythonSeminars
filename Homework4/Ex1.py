5c725d9e74b2c3df6a961e47e28b138e1e38d39e
Timur <47ta@bk.ru>
2022-10-10 19:47:43 +0500

    Homework4

--- Homework4/Ex1.py
+++ Homework4/Ex1.py
@@ -1,3 +1,20 @@
 # Вычислить число c заданной точностью d
 # Пример:
 # при d = 0.001, π = 3.141 10^(-1) ≤ d ≤10^(-10)
+
+def next_leibniz(number, mult):
+    return mult * 4 / number
+
+d = float(input("Введите число d: "))
+multiplier = 1
+divider = 1
+current_leibniz = 1
+answer = 0
+n = 0
+while abs(current_leibniz) > d / 10:
+    n += 1
+    current_leibniz = next_leibniz(divider, multiplier)
+    answer += current_leibniz
+    divider += 2
+    multiplier *= -1
+print(answer)
--- Homework4/Ex2.py
+++ Homework4/Ex2.py
@@ -1,2 +1,14 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+N = int(input("Введите натуральное число N: "))
+
+multiplier = 2
+num_mult = []
+while N > 1:
+    if N % multiplier == 0:
+        num_mult.append(multiplier)
+        N = N / multiplier
+        multiplier = 2
+    else:
+        multiplier += 1
+print(num_mult)
